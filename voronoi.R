0d89a84f1798a8490a03c17cae6e5288b4b10e63
ribeiropvb <65306388+ribeiropvb@users.noreply.github.com>
2023-03-27 07:36:02 -0300

    Update voronoi.R

--- voronoi.R
+++ voronoi.R
@@ -1,37 +1,13 @@
-# install.packages("quantmod")
-library(quantmod)
-
-# Dates
-start <- "2020-10-01"
-end <- "2021-01-01"
-
-# Get the data
-getSymbols("^GSPC", 
-           from = start, to = end,
-           src = "yahoo") 
-
-# Plot the data
-chart_Series(GSPC,
-             TA = list("add_EMA(n = 20, col = 4,
-                                lwd = 2)",
-                       "add_EMA(n = 5, col = 2,
-                                lwd = 2)")) 
-
-
-chartSeries(
-  GSPC,
-  theme = chartTheme("white"),
-  name = "SP500",  
-  TA = list("addBBands(n = 10)",
-            "addVo()",
-            "addEMA(20)",
-            "addEMA(10, col = 2)")
-) 
-
-# install.packages("ggvoronoi")
-# install.packages("ggplot2")
 library(ggvoronoi)
 library(ggplot2)
+
+# Data
+set.seed(1)
+x <- sample(1:400, size = 100)
+y <- sample(1:400, size = 100)
+dist <- sqrt((x - 200) ^ 2 + (y - 200) ^ 2)
+
+df <- data.frame(x, y, dist = dist)
 
 ggplot(df, aes(x, y, fill = dist)) +
   geom_voronoi() +
